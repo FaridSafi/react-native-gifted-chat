1a33ccba90bddd63c07d836677c9a36002dce366
Sam Wenke <wenkesj@users.noreply.github.com>
2015-12-17 12:43:47 -0500

    [GiftedMessenger] Fix issue with RN 0.16.0
    Multiple events propagating, ensure only one goes through to not throw exception.

--- GiftedMessenger.js
+++ GiftedMessenger.js
@@ -336,7 +336,7 @@ var GiftedMessenger = React.createClass({
   
   onKeyboardWillShow(e) {
     Animated.timing(this.state.height, {
-      toValue: this.listViewMaxHeight - e.endCoordinates.height,
+      toValue: this.listViewMaxHeight - (e.endCoordinates ? e.endCoordinates.height : e.end.height),
       duration: 200,
     }).start();
   },

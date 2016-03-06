86a734b020a3f7205dc2ebdb8e95cbddc0594d61
bpeters <brennen.peters@gmail.com>
2016-03-06 15:03:38 -0600

    Expose full rowData to renderCustomText

--- Bubble.js
+++ Bubble.js
@@ -45,7 +45,7 @@ export default class Bubble extends React.Component {
   renderText(text = "", position) {
 
     if (this.props.renderCustomText) {
-      return this.props.renderCustomText(text, position);
+      return this.props.renderCustomText(this.props);
     }
     return (
       <Text style={[styles.text, (position === 'left' ? styles.textLeft : styles.textRight)]}>

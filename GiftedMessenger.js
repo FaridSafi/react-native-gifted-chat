721878abb0e805648c788413ac16b1d492f963fa
bpeters <brennen.peters@gmail.com>
2016-03-14 18:55:10 -0500

    Adjust list height based on changing max height prop

--- GiftedMessenger.js
+++ GiftedMessenger.js
@@ -243,6 +243,10 @@ var GiftedMessenger = React.createClass({
 
     let textInputHeight = this.props.style.textInputContainer.height || 44;
 
+    if (nextProps.maxHeight !== this.props.maxHeight) {
+      this.listViewMaxHeight = nextProps.maxHeight;
+    }
+
     if (nextProps.hideTextInput && !this.props.hideTextInput) {
       this.listViewMaxHeight += textInputHeight;
 

911a3caa105f8db3198db7c1088b9e18c53bab05
Swapnil Singh <mail@swapnilsingh.me>
2016-04-05 17:04:24 +0530

    Adds triangles to bubbles

--- Bubble.js
+++ Bubble.js
@@ -29,7 +29,17 @@ let styles = StyleSheet.create({
   bubbleError: {
     backgroundColor: '#e01717'
   },
-
+  triangleCorner: {
+    position: 'absolute',
+    width: 0,
+    height: 0,
+    backgroundColor: 'transparent',
+    borderStyle: 'solid',
+    borderRightWidth: 10,
+    borderTopWidth: 10,
+    borderRightColor: 'transparent',
+    borderTopColor: '#e6e6eb'
+  }
 });
 
 export default class Bubble extends React.Component {
@@ -54,6 +64,30 @@ export default class Bubble extends React.Component {
     );
   }
 
+  renderTriangle(position) {
+    let triangleStyles = {
+      left: {
+        borderTopColor: (this.props.styles.bubbleLeft.backgroundColor  || '#e6e6eb'),
+        left: -10,
+        bottom: 0,
+        transform: [
+          {rotate: '180deg'}
+        ]
+      },
+      right: {
+        borderTopColor: (this.props.styles.bubbleRight.backgroundColor || '#007aff'),
+        right: -10,
+        bottom: 0,
+        transform: [
+          {rotate: '270deg'}
+        ]
+      }
+    }
+    return (
+      <View style={[styles.triangleCorner, triangleStyles[position]]}/>
+    );
+  }
+
   render(){
     var flexStyle = {};
     if ( this.props.text.length > 40 ) {
@@ -61,12 +95,16 @@ export default class Bubble extends React.Component {
     }
 
     return (
-      <View style={[styles.bubble,
-        (this.props.position === 'left' ? styles.bubbleLeft : styles.bubbleRight),
-        (this.props.status === 'ErrorButton' ? styles.bubbleError : null),
-        flexStyle]}>
-        {this.props.name}
-        {this.renderText(this.props.text, this.props.position)}
+      <View style={styles.bubbleWrapper}>
+        {this.props.position === 'left' ? this.renderTriangle('left') : null}
+        <View style={[styles.bubble,
+          (this.props.position === 'left' ? styles.bubbleLeft : styles.bubbleRight),
+          (this.props.status === 'ErrorButton' ? styles.bubbleError : null),
+          flexStyle]}>
+          {this.props.name}
+          {this.renderText(this.props.text, this.props.position)}
+        </View>
+        {this.props.position === 'right' ? this.renderTriangle('right') : null}
       </View>
     )
   }

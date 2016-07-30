59d73a0c7814deb7d0fcb7a773695f217012967f
Farid from Safi <farid.lazreq@gmail.com>
2016-07-30 21:24:36 +0200

    fix App.js

---
+++ example/App.js
@@ -0,0 +1,146 @@
+import React, { Component } from 'react';
+import {
+  Platform
+} from 'react-native';
+
+import { GiftedChat, Actions, Bubble } from 'react-native-gifted-chat';
+import CustomActions from './CustomActions';
+import CustomView from './CustomView';
+
+export default class Example extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      messages: [],
+      loadEarlier: true,
+    };
+
+    this.onSend = this.onSend.bind(this);
+    this.onReceive = this.onReceive.bind(this);
+    this.renderCustomActions = this.renderCustomActions.bind(this);
+    this.renderBubble = this.renderBubble.bind(this);
+    this.onLoadEarlier = this.onLoadEarlier.bind(this);
+
+    this._isAlright = null;
+  }
+
+  componentWillMount() {
+    this.setState(() => {
+      return {
+        messages: require('./data/messages.js'),
+      };
+    });
+  }
+
+  onLoadEarlier() {
+    this.setState((previousState) => {
+      return {
+        messages: GiftedChat.prepend(previousState.messages, require('./data/old_messages.js')),
+        loadEarlier: false,
+      };
+    });
+  }
+
+  onSend(messages = []) {
+    this.setState((previousState) => {
+      return {
+        messages: GiftedChat.append(previousState.messages, messages),
+      };
+    });
+
+    if (messages.length > 0) {
+      if (messages[0].image) {
+        this.onReceive('Nice picture!');
+      } else if (messages[0].location) {
+        this.onReceive('My favorite place');
+      } else {
+        if (!this._isAlright) {
+          this._isAlright = true;
+          this.onReceive('Alright');
+        }
+      }
+    }
+  }
+
+  onReceive(text) {
+    this.setState((previousState) => {
+      return {
+        messages: GiftedChat.append(previousState.messages, {
+          _id: Math.round(Math.random() * 1000000),
+          text: text,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'React Native',
+            avatar: 'https://facebook.github.io/react/img/logo_og.png',
+          },
+        }),
+      };
+    });
+  }
+
+  renderCustomActions(props) {
+    if (Platform.OS === 'ios') {
+      return (
+        <CustomActions
+          {...props}
+        />
+      );
+    }
+    const options = {
+      'Action 1': (props) => {
+        alert('option 1');
+      },
+      'Action 2': (props) => {
+        alert('option 2');
+      },
+      'Cancel': () => {},
+    };
+    return (
+      <Actions
+        {...props}
+        options={options}
+      />
+    );
+  }
+
+  renderBubble(props) {
+    return (
+      <Bubble
+        {...props}
+        wrapperStyle={{
+          left: {
+            backgroundColor: '#f0f0f0',
+          }
+        }}
+      />
+    );
+  }
+
+  renderCustomView(props) {
+    return (
+      <CustomView
+        {...props}
+      />
+    );
+  }
+
+  render() {
+    return (
+      <GiftedChat
+        messages={this.state.messages}
+        onSend={this.onSend}
+        loadEarlier={this.state.loadEarlier}
+        onLoadEarlier={this.onLoadEarlier}
+
+        user={{
+          _id: 1, // sent messages should have same user._id
+        }}
+
+        renderActions={this.renderCustomActions}
+        renderBubble={this.renderBubble}
+        renderCustomView={this.renderCustomView}
+      />
+    );
+  }
+}

ef42b079dfa0f81ce8ffff52dfdc5af885274687
Nadeesha Cabral <nadeesha.cabral@gmail.com>
2016-11-01 18:49:41 +0530

    refactor: add onChangeVisibleRows listener (#12)
    * refactor: add onChangeVisibleRows listener

--- src/MessageContainer.js
+++ src/MessageContainer.js
@@ -152,6 +152,7 @@ export default class MessageContainer extends React.Component {
           renderHeader={this.renderFooter}
           renderFooter={this.renderLoadEarlier}
           renderScrollComponent={this.renderScrollComponent}
+          onChangeVisibleRows={this.props.onChangeVisibleRows}
         />
       </View>
     );

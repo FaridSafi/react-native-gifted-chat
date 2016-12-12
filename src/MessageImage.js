e84de91ba17aef558ea136658018fd9f43fb7c53
Kevin Cooper <cooperka92@gmail.com>
2016-12-12 15:58:21 -0500

    Add react-native-lightbox for enlarging chat images

--- src/MessageImage.js
+++ src/MessageImage.js
@@ -3,16 +3,27 @@ import {
   Image,
   StyleSheet,
   View,
+  Dimensions,
 } from 'react-native';
+import Lightbox from 'react-native-lightbox';
 
 export default class MessageImage extends React.Component {
   render() {
+    const { width, height } = Dimensions.get('window');
+
     return (
       <View style={[styles.container, this.props.containerStyle]}>
-        <Image
-          style={[styles.image, this.props.imageStyle]}
-          source={{uri: this.props.currentMessage.image}}
-        />
+        <Lightbox
+          navigator={this.props.navigator}
+          activeProps={{
+            style: [styles.imageActive, { width, height }],
+          }}
+        >
+          <Image
+            style={[styles.image, this.props.imageStyle]}
+            source={{uri: this.props.currentMessage.image}}
+          />
+        </Lightbox>
       </View>
     );
   }
@@ -27,6 +38,9 @@ const styles = StyleSheet.create({
     borderRadius: 13,
     margin: 3,
     resizeMode: 'cover',
+  },
+  imageActive: {
+    resizeMode: 'contain',
   },
 });
 

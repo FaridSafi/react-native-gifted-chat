55447aee2a5331cbef04b699187665417098c04f
Charleston Malkemus <cgmalkemus@gmail.com>
2017-01-09 12:23:48 -0500

    Update property for Gifted Chat
    Deprecated property true for keyboardShouldPersistTaps

--- src/GiftedChat.js
+++ src/GiftedChat.js
@@ -68,7 +68,7 @@ class GiftedChat extends React.Component {
 
     this.invertibleScrollViewProps = {
       inverted: true,
-      keyboardShouldPersistTaps: true,
+      keyboardShouldPersistTaps: 'always',
       onTouchStart: this.onTouchStart,
       onTouchMove: this.onTouchMove,
       onTouchEnd: this.onTouchEnd,

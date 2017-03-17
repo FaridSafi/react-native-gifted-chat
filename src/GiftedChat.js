d73284b175b710caae0854a004fa0e1297ca07cd
Thomas Laursen <thomas@addgo.com>
2017-03-17 15:20:24 +0100

    fix for _messageContainerRef === null

--- src/GiftedChat.js
+++ src/GiftedChat.js
@@ -257,6 +257,7 @@ class GiftedChat extends React.Component {
   }
 
   scrollToBottom(animated = true) {
+    if (this._messageContainerRef === null) { return }
     this._messageContainerRef.scrollTo({
       y: 0,
       animated,

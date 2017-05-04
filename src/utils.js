531e71c6de52da2dcfd212f94fb19d40ac40d7fb
abc <abc@abcs-MacBook-Pro-2.local>
2017-05-04 17:55:18 +0530

    fix isSameDay function

--- src/utils.js
+++ src/utils.js
@@ -4,12 +4,12 @@ const DEPRECATION_MESSAGE = 'isSameUser and isSameDay should be imported from th
 
 export function isSameDay(currentMessage = {}, diffMessage = {}) {
 
+  if(!diffMessage.createdAt) {
+    return false
+  }
+
   let currentCreatedAt = moment(currentMessage.createdAt);
   let diffCreatedAt = moment(diffMessage.createdAt);
-
-  if (!currentCreatedAt.isValid() || !diffCreatedAt.isValid()) {
-    return false;
-  }
 
   return currentCreatedAt.isSame(diffCreatedAt, 'day');
 

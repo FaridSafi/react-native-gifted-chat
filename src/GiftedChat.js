a479268a1ddf3746198b5db7b730f5797ba7c214
Nikos Maroulis <nik.maroulis@gmail.com>
2017-06-02 16:21:58 -0700

    increasing the max input

--- src/GiftedChat.js
+++ src/GiftedChat.js
@@ -35,7 +35,7 @@ const MIN_COMPOSER_HEIGHT = Platform.select({
   ios: 33,
   android: 41,
 });
-const MAX_COMPOSER_HEIGHT = 100;
+const MAX_COMPOSER_HEIGHT = 300;
 
 class GiftedChat extends React.Component {
   constructor(props) {

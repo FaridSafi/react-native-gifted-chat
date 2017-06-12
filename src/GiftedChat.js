034fd5a5d0206148d5d3dc244a0589eb0ecbcb7e
Baptiste <nours312@gmail.com>
2017-06-12 22:53:16 +0200

    add renderContainerStyle props on root GiftedChat

--- src/GiftedChat.js
+++ src/GiftedChat.js
@@ -449,7 +449,7 @@ class GiftedChat extends React.Component {
     if (this.state.isInitialized === true) {
       return (
         <ActionSheet ref={component => this._actionSheetRef = component}>
-          <View style={styles.container} onLayout={this.onMainViewLayout}>
+          <View style={[styles.container, this.props.renderContainerStyle]} onLayout={this.onMainViewLayout}>
             {this.renderMessages()}
             {this.renderInputToolbar()}
           </View>
@@ -457,7 +457,7 @@ class GiftedChat extends React.Component {
       );
     }
     return (
-      <View style={styles.container} onLayout={this.onInitialLayoutViewLayout}>
+      <View style={[styles.container, this.props.renderContainerStyle]} onLayout={this.onInitialLayoutViewLayout}>
         {this.renderLoading()}
       </View>
     );

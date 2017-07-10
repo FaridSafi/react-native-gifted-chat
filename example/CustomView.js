cd70add44c176fbca73c1be5f05c9649dd9cb45c
Kevin Cooper <cooperka92@gmail.com>
2017-07-09 19:18:01 -0700

    [Example] Migrate to react-native-maps (MapView was deprecated)

--- example/CustomView.js
+++ example/CustomView.js
@@ -1,12 +1,12 @@
 import React from 'react';
 import {
   Linking,
-  MapView,
   Platform,
   StyleSheet,
   TouchableOpacity,
   ViewPropTypes,
 } from 'react-native';
+import MapView from 'react-native-maps';
 
 export default class CustomView extends React.Component {
   render() {
@@ -30,11 +30,9 @@ export default class CustomView extends React.Component {
             region={{
               latitude: this.props.currentMessage.location.latitude,
               longitude: this.props.currentMessage.location.longitude,
+              latitudeDelta: 0.0922,
+              longitudeDelta: 0.0421,
             }}
-            annotations={[{
-              latitude: this.props.currentMessage.location.latitude,
-              longitude: this.props.currentMessage.location.longitude,
-            }]}
             scrollEnabled={false}
             zoomEnabled={false}
           />

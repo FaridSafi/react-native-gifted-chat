e177c392e9cc23417536199e5c68e1cedef7c525
apparition <aaron@onefatgiraffe.com>
2017-12-07 17:36:40 +0900

    loadEarlier Component always at top irregardless of props.inverted value

--- src/MessageContainer.js
+++ src/MessageContainer.js
@@ -156,8 +156,8 @@ export default class MessageContainer extends React.Component {
           dataSource={this.state.dataSource}
 
           renderRow={this.renderRow}
-          renderHeader={this.renderFooter}
-          renderFooter={this.renderLoadEarlier}
+          renderHeader={this.props.inverted ? this.renderFooter : this.renderLoadEarlier}
+          renderFooter={this.props.inverted ? this.renderLoadEarlier : this.renderFooter}
           renderScrollComponent={this.renderScrollComponent}
         />
       </View>

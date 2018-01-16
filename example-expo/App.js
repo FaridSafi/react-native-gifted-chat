91adad0656204bf9e33269cd3d643387d02d0453
Xavier Carpentier <xcapetir@gmail.com>
2018-01-16 18:21:26 +0100

    feat(appr and expo): give it a try

---
+++ example-expo/App.js
@@ -0,0 +1,22 @@
+/* eslint-disable */
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
+
+export default class App extends React.Component {
+  render() {
+    return (
+      <View style={styles.container}>
+        <Text>Open up App.js to start working on your app!</Text>
+      </View>
+    );
+  }
+}

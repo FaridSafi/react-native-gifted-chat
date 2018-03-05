59a8075c5f29641933527225e29b24c0685c3056
Martin Adamko <martin.adamko@technologystudio.sk>
2018-03-05 10:10:16 +0100

    Update to pass tests

--- src/InputToolbar.js
+++ src/InputToolbar.js
@@ -2,7 +2,7 @@
 
 import PropTypes from 'prop-types';
 import React from 'react';
-import { StyleSheet, View, Keyboard, ViewPropTypes, Dimensions } from 'react-native';
+import { StyleSheet, View, Keyboard, ViewPropTypes } from 'react-native';
 
 import Composer from './Composer';
 import Send from './Send';

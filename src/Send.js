a830d65cb60ca17814a35224df2f1bc0e81ceed7
Aaron Lee <apparition47@users.noreply.github.com>
2018-03-27 04:08:07 +0900

    Add prop to always show send button (#717)
    * Add prop to always show send button
    
    * Clarify alwaysShowSend usage

--- src/Send.js
+++ src/Send.js
@@ -5,8 +5,8 @@ import React from 'react';
 import { StyleSheet, Text, TouchableOpacity, View, ViewPropTypes } from 'react-native';
 import Color from './Color';
 
-export default function Send({ text, containerStyle, onSend, children, textStyle, label }) {
-  if (text.trim().length > 0) {
+export default function Send({ text, containerStyle, onSend, children, textStyle, label, alwaysShowSend }) {
+  if (alwaysShowSend || text.trim().length > 0) {
     return (
       <TouchableOpacity
         style={[styles.container, containerStyle]}
@@ -45,6 +45,7 @@ Send.defaultProps = {
   containerStyle: {},
   textStyle: {},
   children: null,
+  alwaysShowSend: false,
 };
 
 Send.propTypes = {
@@ -54,4 +55,5 @@ Send.propTypes = {
   containerStyle: ViewPropTypes.style,
   textStyle: Text.propTypes.style,
   children: PropTypes.element,
+  alwaysShowSend: PropTypes.bool,
 };

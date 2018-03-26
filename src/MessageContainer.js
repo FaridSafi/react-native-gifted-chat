75f9ac879629a6cde3686fdb9b55369ed291b84e
Valtteri Laine <valtteri@meruhealth.com>
2018-03-26 13:32:26 +0300

    Allow overwriting contentContainerStyle

--- src/MessageContainer.js
+++ src/MessageContainer.js
@@ -154,9 +154,9 @@ export default class MessageContainer extends React.Component {
           automaticallyAdjustContentInsets={false}
           initialListSize={20}
           pageSize={20}
+          contentContainerStyle={contentContainerStyle}
           {...this.props.listViewProps}
           dataSource={this.state.dataSource}
-          contentContainerStyle={contentContainerStyle}
           renderRow={this.renderRow}
           renderHeader={this.props.inverted ? this.renderFooter : this.renderLoadEarlier}
           renderFooter={this.props.inverted ? this.renderLoadEarlier : this.renderFooter}

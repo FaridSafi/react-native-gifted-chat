b4de2ea140dca9be8d53064d70ce1f51cc9a6ab8
imcodervn <33474102+imcodervn@users.noreply.github.com>
2018-06-01 11:37:43 +0700

    Update utils.js

--- src/utils.js
+++ src/utils.js
@@ -1,4 +1,4 @@
-import moment from 'moment';
+import moment from 'moment/min/moment-with-locales';
 
 export function isSameDay(currentMessage = {}, diffMessage = {}) {
   if (!diffMessage.createdAt) {

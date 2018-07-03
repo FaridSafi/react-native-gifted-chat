1fe8d770e8a6f818cae6adbdc1724081230470f3
YuriiStetsula <s.yuri@ukr.net>
2018-07-03 10:34:07 +0300

    fix typo

--- index.d.ts
+++ index.d.ts
@@ -230,7 +230,7 @@ export interface GiftedChatProps {
   maxInputLength?: number;
   /* Custom parse patterns for react-native-parsed-text used to linkify message content (like URLs and phone numbers) */
   parsePatterns?(): React.ReactNode;
-  /* Posibility to switch off keyboard avoiding, default is true */ 
+  /* Possibility to switch off keyboard avoiding, default is true */ 
   avoidKeyboard?: boolean
 }
 

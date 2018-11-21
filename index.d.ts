0a253f0f2bacc49674a4856943cac88a956a6f63
Xavier Carpentier <xcapetir@gmail.com>
2018-11-21 11:47:37 +0100

    fix imageStyle?: ViewStyle

--- index.d.ts
+++ index.d.ts
@@ -237,7 +237,7 @@ export interface GiftedChatProps {
   /* Force send button */
   alwaysShowSend?: boolean;
   /* Image style */
-  imageStyle: ViewStyle
+  imageStyle?: ViewStyle
 }
 
 export class GiftedChat extends React.Component<GiftedChatProps> {

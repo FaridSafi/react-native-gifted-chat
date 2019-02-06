a0cba453dccea9f40ee1af9d88048c6efd83dffb
Xavier Carpentier <xcapetir@gmail.com>
2019-02-06 10:22:02 +0100

    fix scu sent instead of send and add audio

--- src/Message.js
+++ src/Message.js
@@ -40,13 +40,14 @@ export default class Message extends React.Component {
     const { nextMessage } = this.props;
     const nextPropsMessage = nextProps.nextMessage;
     return (
-      next.send !== current.send ||
+      next.sent !== current.sent ||
       next.received !== current.received ||
       next.pending !== current.pending ||
       next.createdAt !== current.createdAt ||
       next.text !== current.text ||
       next.image !== current.image ||
       next.video !== current.video ||
+      next.audio !== current.audio ||
       nextMessage !== nextPropsMessage
     );
   }

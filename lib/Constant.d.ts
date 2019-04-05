a202b90d2d866b19e1ad4e0b7ab75edb023d560f
Xavier Carpentier <xcapetir@gmail.com>
2019-04-05 18:15:46 +0200

    remove lib

--- lib/Constant.d.ts
+++
@@ -1,5 +0,0 @@
-export declare const MIN_COMPOSER_HEIGHT: number;
-export declare const MAX_COMPOSER_HEIGHT = 200;
-export declare const DEFAULT_PLACEHOLDER = "Type a message...";
-export declare const DATE_FORMAT = "ll";
-export declare const TIME_FORMAT = "LT";
--- lib/utils.d.ts
+++
@@ -1,3 +0,0 @@
-import { IMessage } from './types';
-export declare function isSameDay(currentMessage: IMessage, diffMessage: IMessage): boolean;
-export declare function isSameUser(currentMessage: IMessage, diffMessage: IMessage): boolean;

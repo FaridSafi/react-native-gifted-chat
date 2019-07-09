ad7539ef24154360d1e0d238134dcf37ffa2079c
Web Premiere <david@web-premiere.fr>
2019-07-09 10:58:07 +0200

    Allow web platform
    Allow to work on web platform

--- src/Constant.ts
+++ src/Constant.ts
@@ -3,6 +3,7 @@ import { Platform } from 'react-native';
 export const MIN_COMPOSER_HEIGHT = Platform.select({
   ios: 33,
   android: 41,
+  web: 20
 });
 export const MAX_COMPOSER_HEIGHT = 200;
 export const DEFAULT_PLACEHOLDER = 'Type a message...';

5816e0a1e537a68320d31b07e37e879d0aec53dc
Xavier Carpentier <xcapetir@gmail.com>
2019-07-22 17:36:09 +0200

    fix perf scroll

--- example-expo/data/messages.js
+++ example-expo/data/messages.js
@@ -218,59 +218,59 @@ export default [
       name: 'Developer',
     },
   },
-  // {
-  //   _id: 10,
-  //   text: 'This is a quick reply. Do you love Gifted Chat? (radio) KEEP IT',
-  //   createdAt: new Date(),
-  //   quickReplies: {
-  //     type: 'radio', // or 'checkbox',
-  //     keepIt: true,
-  //     values: [
-  //       {
-  //         title: '😋 Yes',
-  //         value: 'yes',
-  //       },
-  //       {
-  //         title: '📷 Yes, let me show you with a picture!',
-  //         value: 'yes_picture',
-  //       },
-  //       {
-  //         title: '😞 Nope. What?',
-  //         value: 'no',
-  //       },
-  //     ],
-  //   },
-  //   user: {
-  //     _id: 2,
-  //     name: 'React Native',
-  //   },
-  // },
-  // {
-  //   _id: 20,
-  //   text: 'This is a quick reply. Do you love Gifted Chat? (checkbox)',
-  //   createdAt: new Date(),
-  //   quickReplies: {
-  //     type: 'checkbox', // or 'checkbox',
-  //     values: [
-  //       {
-  //         title: 'Yes',
-  //         value: 'yes',
-  //       },
-  //       {
-  //         title: 'Yes, let me show you with a picture!',
-  //         value: 'yes_picture',
-  //       },
-  //       {
-  //         title: 'Nope. What?',
-  //         value: 'no',
-  //       },
-  //     ],
-  //   },
-  //   user: {
-  //     _id: 2,
-  //     name: 'React Native',
-  //   },
-  // },
+  {
+    _id: 10,
+    text: 'This is a quick reply. Do you love Gifted Chat? (radio) KEEP IT',
+    createdAt: new Date(),
+    quickReplies: {
+      type: 'radio', // or 'checkbox',
+      keepIt: true,
+      values: [
+        {
+          title: '😋 Yes',
+          value: 'yes',
+        },
+        {
+          title: '📷 Yes, let me show you with a picture!',
+          value: 'yes_picture',
+        },
+        {
+          title: '😞 Nope. What?',
+          value: 'no',
+        },
+      ],
+    },
+    user: {
+      _id: 2,
+      name: 'React Native',
+    },
+  },
+  {
+    _id: 20,
+    text: 'This is a quick reply. Do you love Gifted Chat? (checkbox)',
+    createdAt: new Date(),
+    quickReplies: {
+      type: 'checkbox', // or 'checkbox',
+      values: [
+        {
+          title: 'Yes',
+          value: 'yes',
+        },
+        {
+          title: 'Yes, let me show you with a picture!',
+          value: 'yes_picture',
+        },
+        {
+          title: 'Nope. What?',
+          value: 'no',
+        },
+      ],
+    },
+    user: {
+      _id: 2,
+      name: 'React Native',
+    },
+  },
   // {
   //   _id: 9,
   //   text: 'You are officially rocking GiftedChat.',

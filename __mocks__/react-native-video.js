0a1af63654288ef9d129d91fc9ac97952a02138a
Xavier Carpentier <xcapetir@gmail.com>
2019-09-19 10:47:47 +0200

    fix re-add rn-video mock

---
+++ __mocks__/react-native-video.js
@@ -0,0 +1,7 @@
+// __mocks__/react-native-video.js
+import React from 'react'
+import { View } from 'react-native'
+
+// Import this named export into your test file:
+const Video = () => <View>React native video mock</View>
+export default Video

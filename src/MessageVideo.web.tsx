2599afc69eca32df942c7fc70582cc07fc022cfd
Xavier Carpentier <xcapetir@gmail.com>
2019-09-19 16:09:34 +0200

    fix video issue on web

--- src/MessageVideo.web.tsx
+++ src/MessageVideo.web.tsx
@@ -1,2 +1,11 @@
-import MessageVideo from './ExpoMessageVideo'
-export default MessageVideo
+import React from 'react'
+import Color from './Color'
+import { View, Text } from 'react-native'
+
+export default () => (
+  <View style={{ padding: 20 }}>
+    <Text style={{ color: Color.alizarin, fontWeight: '600' }}>
+      Video is not yet implemented for the web
+    </Text>
+  </View>
+)

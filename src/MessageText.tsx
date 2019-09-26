4790fccce96161e804dcda6a8c847f8d3df51947
sn-axellero <55737874+sn-axellero@users.noreply.github.com>
2019-09-26 14:25:19 +0200

    Update MessageText.tsx

--- src/MessageText.tsx
+++ src/MessageText.tsx
@@ -183,7 +183,7 @@ export default class MessageText<
             this.props.customTextStyle,
           ]}
           parse={[
-            ...this.props.parsePatterns(linkStyle),
+            ...this.props.parsePatterns!(linkStyle as TextStyle),
             { type: 'url', style: linkStyle, onPress: this.onUrlPress },
             { type: 'phone', style: linkStyle, onPress: this.onPhonePress },
             { type: 'email', style: linkStyle, onPress: this.onEmailPress },

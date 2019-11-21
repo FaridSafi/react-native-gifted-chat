527a498527ef50a35421c30c6b473e610affca2e
Kafil Uddin Kiron <kafiluddin.kiron@gmail.com>
2019-11-21 10:42:07 +0600

    adjust message container height

--- src/GiftedChat.tsx
+++ src/GiftedChat.tsx
@@ -433,6 +433,14 @@ class GiftedChat<TMessage extends IMessage = IMessage> extends React.Component<
     if (text !== prevProps.text) {
       this.setTextFromProp(text)
     }
+
+    if (prevProps.minInputToolbarHeight !== this.props.minInputToolbarHeight) {
+      this.setState({
+        messagesContainerHeight: this.prepareMessagesContainerHeight(
+          this.getBasicMessagesContainerHeight(),
+        ),
+      })
+    }
   }
 
   initLocale() {

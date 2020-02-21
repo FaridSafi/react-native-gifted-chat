40b2294a271104155d5ea3b8e9e12c07046e08e5
William Staggs <William@staggs.io>
2020-02-21 14:12:13 -0600

    Add tests for isSameDay changes

--- src/__tests__/utils.test.ts
+++ src/__tests__/utils.test.ts
@@ -20,6 +20,54 @@ it('should test if same day', () => {
   ).toBe(true)
 })
 
+it('test both undefined createdAt for same day', () => {
+  expect(
+    isSameDay({
+      _id: 1,
+      text: 'test',
+      user: {_id: 1}
+    }, {
+      _id: 2,
+      text: 'test2',
+      user: {_id: 2}
+    })
+  ).toBe(true)
+})
+
+it('should test undefined currentMessage.createdAt for same day', () => {
+  const now = new Date()
+
+  expect(
+    isSameDay({
+      _id: 1,
+      text: 'test',
+      user: {_id: 1}
+    }, {
+      _id: 2,
+      text: 'test2',
+      createdAt: now,
+      user: {_id: 2}
+    })
+  ).toBe(false)
+})
+
+it('should test undefined diffMessage.createdAt for same day', () => {
+  const now = new Date()
+
+  expect(
+    isSameDay({
+      _id: 1,
+      text: 'test',
+      createdAt: now,
+      user: {_id: 1}
+    }, {
+      _id: 2,
+      text: 'test2',
+      user: {_id: 2}
+    })
+  ).toBe(true)
+})
+
 it('should test if same user', () => {
   const message = {
     _id: 1,

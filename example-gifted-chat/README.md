c9b3a64321e4b3fa5784dc989b5d36071fac9cff
Aaron Lu <aaron.lu@mvpfastlane.com>
2020-04-09 15:59:41 +0800

    Example for using the renderProps of gifted-chat

---
+++ example-gifted-chat/README.md
@@ -0,0 +1,27 @@
+# example-gifted-chat
+
+Lots of people using `react-native-gifted-chat` might want to know that...
+
+1. There are so many render props could use, but what should I pass?
+2. How could I customize each component, but leaving its functionality.
+
+> For example, said that you want to customize the `send button`, when you pass your own component to `renderSend`, after that you will lose the functionality of clean up text when a message has been sent.
+
+---
+
+I made this for anyone who wants to know how to use the render Props properly.
+
+##### Such as:
+
+- renderInputToolbar
+- renderActions
+- renderComposer
+- renderSend
+- renderAvatar
+- renderBubble
+- renderSystemMessage
+- renderMessage
+- renderMessageText
+- renderCustomView
+
+<img src="https://i.imgur.com/dbkc7I4.png" alt="Example for customize components" width="300">
---
+++ example-gifted-chat/src/MessageContainer.js
@@ -0,0 +1,91 @@
+/* eslint-disable react/jsx-props-no-spreading */
+import React from 'react';
+import { View, Text } from 'react-native';
+import { Avatar, Bubble, SystemMessage, Message, MessageText } from 'react-native-gifted-chat';
+
+export const renderAvatar = (props) => (
+  <Avatar
+    {...props}
+    containerStyle={{ left: { borderWidth: 3, borderColor: 'red' }, right: {} }}
+    imageStyle={{ left: { borderWidth: 3, borderColor: 'blue' }, right: {} }}
+  />
+);
+
+export const renderBubble = (props) => (
+  <Bubble
+    {...props}
+    // renderTime={() => <Text>Time</Text>}
+    // renderTicks={() => <Text>Ticks</Text>}
+    containerStyle={{
+      left: { borderColor: 'teal', borderWidth: 8 },
+      right: {},
+    }}
+    wrapperStyle={{
+      left: { borderColor: 'tomato', borderWidth: 4 },
+      right: {},
+    }}
+    bottomContainerStyle={{
+      left: { borderColor: 'purple', borderWidth: 4 },
+      right: {},
+    }}
+    tickStyle={{}}
+    usernameStyle={{ color: 'tomato', fontWeight: '100' }}
+    containerToNextStyle={{
+      left: { borderColor: 'navy', borderWidth: 4 },
+      right: {},
+    }}
+    containerToPreviousStyle={{
+      left: { borderColor: 'mediumorchid', borderWidth: 4 },
+      right: {},
+    }}
+  />
+);
+
+export const renderSystemMessage = (props) => (
+  <SystemMessage
+    {...props}
+    containerStyle={{ backgroundColor: 'pink' }}
+    wrapperStyle={{ borderWidth: 10, borderColor: 'white' }}
+    textStyle={{ color: 'crimson', fontWeight: '900' }}
+  />
+);
+
+export const renderMessage = (props) => (
+  <Message
+    {...props}
+    // renderDay={() => <Text>Date</Text>}
+    containerStyle={{
+      left: { backgroundColor: 'lime' },
+      right: { backgroundColor: 'gold' },
+    }}
+  />
+);
+
+export const renderMessageText = (props) => (
+  <MessageText
+    {...props}
+    containerStyle={{
+      left: { backgroundColor: 'yellow' },
+      right: { backgroundColor: 'purple' },
+    }}
+    textStyle={{
+      left: { color: 'red' },
+      right: { color: 'green' },
+    }}
+    linkStyle={{
+      left: { color: 'orange' },
+      right: { color: 'orange' },
+    }}
+    customTextStyle={{ fontSize: 24, lineHeight: 24 }}
+  />
+);
+
+export const renderCustomView = ({ user }) => (
+  <View style={{ minHeight: 20, alignItems: 'center' }}>
+    <Text>
+      Current user:
+      {user.name}
+    </Text>
+    <Text>From CustomView</Text>
+  </View>
+);

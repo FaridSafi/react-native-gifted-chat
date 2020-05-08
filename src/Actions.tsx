293053231a603dd573c4f16404d4e39f46a3b206
Xavier Carpentier <xcapetir@gmail.com>
2020-05-08 16:03:38 +0200

    feat remove more old context

--- src/Actions.tsx
+++ src/Actions.tsx
@@ -11,6 +11,8 @@ import {
 } from 'react-native'
 import Color from './Color'
 import { StylePropType } from './utils'
+import { useChatContext } from './GiftedChatContext'
+import { useCallbackOne } from 'use-memo-one'
 
 export interface ActionsProps {
   options?: { [key: string]: any }
@@ -22,70 +24,72 @@ export interface ActionsProps {
   onPressActionButton?(): void
 }
 
-export default class Actions extends React.Component<ActionsProps> {
-  static defaultProps: ActionsProps = {
-    options: {},
-    optionTintColor: Color.optionTintColor,
-    icon: undefined,
-    containerStyle: {},
-    iconTextStyle: {},
-    wrapperStyle: {},
-  }
-
-  static propTypes = {
-    onSend: PropTypes.func,
-    options: PropTypes.object,
-    optionTintColor: PropTypes.string,
-    icon: PropTypes.func,
-    onPressActionButton: PropTypes.func,
-    wrapperStyle: StylePropType,
-    containerStyle: StylePropType,
-  }
-
-  static contextTypes = {
-    actionSheet: PropTypes.func,
-  }
-
-  onActionsPress = () => {
-    const { options } = this.props
+export const Actions = ({
+  options,
+  optionTintColor,
+  icon,
+  wrapperStyle,
+  iconTextStyle,
+  onPressActionButton,
+  containerStyle,
+}: ActionsProps) => {
+  const { actionSheet } = useChatContext()
+  const onActionsPress = useCallbackOne(() => {
     const optionKeys = Object.keys(options!)
     const cancelButtonIndex = optionKeys.indexOf('Cancel')
-    this.context.actionSheet().showActionSheetWithOptions(
+    actionSheet().showActionSheetWithOptions(
       {
         options: optionKeys,
         cancelButtonIndex,
-        tintColor: this.props.optionTintColor,
+        tintColor: optionTintColor,
       },
       (buttonIndex: number) => {
         const key = optionKeys[buttonIndex]
         if (key) {
-          options![key](this.props)
+          options![key]()
         }
       },
     )
-  }
+  }, [])
 
-  renderIcon() {
-    if (this.props.icon) {
-      return this.props.icon()
+  const renderIcon = useCallbackOne(() => {
+    if (icon) {
+      return icon()
     }
     return (
-      <View style={[styles.wrapper, this.props.wrapperStyle]}>
-        <Text style={[styles.iconText, this.props.iconTextStyle]}>+</Text>
+      <View style={[styles.wrapper, wrapperStyle]}>
+        <Text style={[styles.iconText, iconTextStyle]}>+</Text>
       </View>
     )
-  }
+  }, [])
 
-  render() {
-    return (
-      <TouchableOpacity
-        style={[styles.container, this.props.containerStyle]}
-        onPress={this.props.onPressActionButton || this.onActionsPress}
-      >
-        {this.renderIcon()}
-      </TouchableOpacity>
-    )
-  }
+  return (
+    <TouchableOpacity
+      style={[styles.container, containerStyle]}
+      onPress={onPressActionButton || onActionsPress}
+    >
+      {renderIcon()}
+    </TouchableOpacity>
+  )
+}
+
+Actions.defaultProps = {
+  options: {},
+  optionTintColor: Color.optionTintColor,
+  icon: undefined,
+  containerStyle: {},
+  iconTextStyle: {},
+  wrapperStyle: {},
+}
+
+Actions.propTypes = {
+  onSend: PropTypes.func,
+  options: PropTypes.object,
+  optionTintColor: PropTypes.string,
+  icon: PropTypes.func,
+  onPressActionButton: PropTypes.func,
+  wrapperStyle: StylePropType,
+  containerStyle: StylePropType,
 }
 
 const styles = StyleSheet.create({

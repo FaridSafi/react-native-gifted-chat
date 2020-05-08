f7814bbf7408af9606c9b20382121d2acacf5dfe
Xavier Carpentier <xcapetir@gmail.com>
2020-05-08 15:46:39 +0200

    fix remove old fashion context from test too

--- src/__tests__/Day.test.tsx
+++ src/__tests__/Day.test.tsx
@@ -1,11 +1,11 @@
 import 'react-native'
 import React from 'react'
-import createComponentWithContext from './context'
+import renderer from 'react-test-renderer'
 
 import { Day } from '../GiftedChat'
 
 it('should render <Day /> and compare with snapshot', () => {
-  const component = createComponentWithContext(<Day />)
+  const component = renderer.create(<Day />)
   const tree = component.toJSON()
 
   expect(tree).toMatchSnapshot()

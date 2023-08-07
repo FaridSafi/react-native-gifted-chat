5b915bd01514a1d5dd57d358cd1926c43d7c2860
Philippe de Reynal <philippe@dereynal.com>
2023-08-07 22:59:16 +0200

    fix import uuid v4 and upgrade module

--- src/GiftedChat.tsx
+++ src/GiftedChat.tsx
@@ -21,7 +21,7 @@ import {
   ViewStyle,
 } from 'react-native'
 import { LightboxProps } from 'react-native-lightbox-v2'
-import uuid from 'uuid'
+import { v4 as uuidV4 } from 'uuid'
 import { Actions, ActionsProps } from './Actions'
 import { Avatar, AvatarProps } from './Avatar'
 import Bubble from './Bubble'
@@ -236,7 +236,7 @@ function GiftedChat<TMessage extends IMessage = IMessage>(
     text = undefined,
     initialText = '',
     isTyping,
-    messageIdGenerator = () => uuid.v4(),
+    messageIdGenerator = () => uuidV4(),
     user = {},
     onSend = () => {},
     locale = 'en',

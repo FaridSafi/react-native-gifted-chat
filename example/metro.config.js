6b3dac9a27c11ed124d9fe9c7aabd53f40346c01
Kesha Antonov <innokenty.longway@gmail.com>
2024-04-05 15:50:14 +0300

    example: upd deps

--- example/metro.config.js
+++ example/metro.config.js
@@ -1,26 +1,34 @@
-const { getDefaultConfig } = require('@expo/metro-config')
+const { getDefaultConfig, mergeConfig } = require('@react-native/metro-config')
+
+/**
+ * Metro configuration
+ * https://facebook.github.io/metro/docs/configuration
+ *
+ * @type {import('metro-config').MetroConfig}
+ */
+
 const path = require('path')
+const config = {};
 
-const defaultConfig = getDefaultConfig(__dirname)
-
-defaultConfig.watchFolders = [
-  ...defaultConfig.watchFolders,
+config.watchFolders = [
   path.resolve(__dirname, '../src'),
 ]
 
-defaultConfig.resolver.extraNodeModules = new Proxy(
-  {},
-  {
-    get: (target, name) => {
-      if (target.hasOwnProperty(name)) {
-        return target[name]
-      }
-      if (name === 'react-native-gifted-chat') {
-        return path.join(process.cwd(), `../src`)
-      }
-      return path.join(process.cwd(), `node_modules/${name}`)
+config.resolver = {
+  extraNodeModules: new Proxy(
+    {},
+    {
+      get: (target, name) => {
+        if (target.hasOwnProperty(name)) {
+          return target[name]
+        }
+        if (name === 'react-native-gifted-chat') {
+          return path.join(process.cwd(), `../src`)
+        }
+        return path.join(process.cwd(), `node_modules/${name}`)
+      },
     },
-  },
-)
+  ),
+}
 
-module.exports = defaultConfig
+module.exports = mergeConfig(getDefaultConfig(__dirname), config);

422524626da06356da1e4cffcd2322aa57d0db5f
Kesha Antonov <innokenty.longway@gmail.com>
2024-08-27 23:19:57 +0300

    rm expo-clipboard mock

--- tests/setup.js
+++ tests/setup.js
@@ -2,5 +2,4 @@ require('react-native-reanimated').setUpTests()
 
 // mocks
 jest.mock('@expo/react-native-action-sheet', () => 'ActionSheet')
-jest.mock('expo-clipboard', () => 'Clipboard')
 jest.mock('react-native-lightbox-v2', () => 'Lightbox')

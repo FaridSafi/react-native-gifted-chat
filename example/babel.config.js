4d18ae8cd8cc6fde25d5e9eaca0f6c0c97df0f4c
Kesha Antonov <innokenty.longway@gmail.com>
2025-11-15 20:36:42 +0300

    example: setup lo point local repo

---
+++ example/babel.config.js
@@ -0,0 +1,19 @@
+console.log('babel')
+
+module.exports = function (api) {
+  api.cache(true);
+  return {
+    presets: ['babel-preset-expo'],
+    plugins: [
+      [
+        'module-resolver', {
+          root: ['.'],
+          alias: {
+            'react-native-gifted-chat': '../src',
+          },
+        },
+      ],
+      'react-native-reanimated/plugin',
+    ],
+  };
+};
